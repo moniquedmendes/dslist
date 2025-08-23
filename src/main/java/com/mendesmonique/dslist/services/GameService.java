3224b31a274f85c72363212edeebe9838145e21f
moniquedmendes <moniquedmendes@gmail.com>
2025-08-23 14:18:05 -0300

    buscar por id

--- src/main/java/com/mendesmonique/dslist/services/GameService.java
+++ src/main/java/com/mendesmonique/dslist/services/GameService.java
@@ -2,10 +2,11 @@ package com.mendesmonique.dslist.services;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mendesmonique.dslist.dto.GameDTO;
 import com.mendesmonique.dslist.dto.GameMinDTO;
 import com.mendesmonique.dslist.entities.Game;
 import com.mendesmonique.dslist.repositories.GameRepository;
@@ -16,6 +17,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long Id) {
+		Game result = gameRepository.findById(Id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();		
