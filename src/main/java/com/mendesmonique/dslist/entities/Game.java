b48019f897462082d87c0349c6d39b1adda795e3
moniquedmendes <moniquedmendes@gmail.com>
2025-08-22 21:27:59 -0300

    BD Games

--- src/main/java/com/mendesmonique/dslist/entities/Game.java
+++ src/main/java/com/mendesmonique/dslist/entities/Game.java
@@ -22,8 +22,13 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
@@ -37,6 +42,7 @@ public class Game {
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -80,6 +86,14 @@ public class Game {
 
 	public void setPlatforms(String platforms) {
 		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
